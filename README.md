a809c2752e9789d48d4eef564057f293857a55cb
Julian Ichich <julianorlandoichich@gmail.com>
2025-03-20 22:45:07 -0600

    Forzar eliminación de archivo con nombre conflictivo

--- README.md
+++
@@ -1,3 +0,0 @@
-# este es mi primer proyecto con React.js
-
-usando React.js he hecho un proyecto de ToDo.
--- src/TodoIcon/deleteIcon.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { TodoICon } from '.'
-function DeleteIcon({onDelete}){
-    return (
-        <TodoICon type='delete' color='gray' onClick={onDelete} />
-    )
-}
-
-export { DeleteIcon }
